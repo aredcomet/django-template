4d1d64abbfd78a71971928185de30e0ef0b38ac4
Abhishek <abhishek.nhs@outlook.com>
2022-05-20 15:38:02 +0530

    Inc: phone number field and meta

--- {{cookiecutter.project_slug}}/backend/core/models.py
+++ {{cookiecutter.project_slug}}/backend/core/models.py
@@ -7,7 +7,14 @@ class User(AbstractUser):
     last_name = None
 
     name = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     avatar = models.ImageField(null=True)
+
+    class Meta:
+        verbose_name_plural = 'Users'
+        ordering = [
+            "-id",
+        ]
 
     def __str__(self) -> str:
         return self.username
