bc40f1123a9c532ce7ac5b66c34be29fd5d7b7a7
Abhishek <abhishek.nhs@outlook.com>
2022-06-26 22:26:04 +0530

    Inc: Bearer token (prefix) support

--- {{cookiecutter.project_slug}}/backend/backend/settings.py
+++ {{cookiecutter.project_slug}}/backend/backend/settings.py
@@ -189,6 +189,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "backend.token_auth.BearerTokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
